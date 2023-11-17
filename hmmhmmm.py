a99203d531f40e83e50e1aa333c52bd65cfee2d0
Kira-Stuber <ks120508@wgcloud.org>
2023-11-17 11:24:25 -0600

    finishing soon

--- hmmhmmm.py
+++ hmmhmmm.py
@@ -1,39 +1,52 @@
 
-action = input("Do you want to add or view an entry?: ")
+dict_entry = {}
+while True:
+    print("Mainscreen")
+    action = input("Do you want to add or view an entry?: ")
 
-if action == "add" or action == "Add" or action == "add an entry" or action == "Add an entry":
-    entry_num = int(input("How many entries do you want to add?: "))
-    dict_entry = {}
-    for x in range(entry_num):
+    if action == "add" or action == "Add" or action == "add an entry" or action == "Add an entry":
+        entry_num = int(input("How many entries do you want to add?: "))
+        for x in range(entry_num):
+            print()
+            x = x + 1
+            x = str(x)
+            print("Entry #" + x)
+            print()
+            date = input("What is the date?: ")
+            entry = input("I am thankful for... ")
+            fullentry = date + ": I am thankful for " + entry
+            dict_entry[date] = fullentry
+
         print()
-        printX = x + 1
-        printX = str(printX)
-        print("Entry #" + printX)
-        print()
-        date = input("What is the date?: ")
-        entry = input("I am thankful for... ")
-        fullentry = date + ": I am thankful for " + entry
-        dict_entry[x] = fullentry
+        returnn = input("Do you want to return to the main screen?: ")
+        if returnn == "yes" or returnn == "Yes":
+            print()
+            continue
+        else:
+            break
 
-i = int(input("Which entry do you want to see?: "))
-i = i - 1
-print(dict_entry[i])
+    if action == "view" or action == "View" or action == "view an entry" or action == "View an entry":
+        while True:
+            False
+            i = input("Which entry do you want to see? (date): ")
+            if dict_entry == {}:
+                print()
+                print("That entry doesn't exist.")
+            else:
+                print()
+                print(dict_entry[i])
+                print()
 
-if action == "view" or action == "View" or action == "view an entry" or action == "View an entry":
-    i = int(input("Which entry do you want to see?: "))
-    i = i - 1
-    print(dict_entry[i])
-
-
-
-
-"""
-cooking = input("Are you cooking a turkey?: ")
-if cooking == "yes" or cooking == "Yes":
-    weight = float(input("What is its weight in pounds?: "))
-    time = weight * 20
-    if time % 1 == 0:
-        time = int(time)
-    time = str(time)
-    print("You should cook the turkey for " + time + " minutes.")
-"""
+            print()
+            repeat = input("Do you want to review any other entries?: ")
+            if repeat == "yes" or repeat == "Yes":
+                True
+            else:
+                break      
+        
+        returnn = input("Do you want to return to the main screen?: ")
+        if returnn == "yes" or returnn == "Yes":
+            print()
+            continue
+        else:
+            break
